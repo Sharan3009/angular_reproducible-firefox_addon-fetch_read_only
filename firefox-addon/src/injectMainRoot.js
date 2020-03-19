0ffe84de6fca831b3baa72a45885369721287379
Sharandeep Singh <>
2020-03-19 21:11:12 +0530

    first commit

---
+++ firefox-addon/src/injectMainRoot.js
@@ -0,0 +1,6 @@
+let panel = document.createElement("app-root");
+try {
+    document.body.appendChild(panel);
+} catch (e) {
+    console.log(e);
+}
---
+++ webapp/elements-build.js
@@ -0,0 +1,18 @@
+const concat = require('concat');
+const replace = require('replace-in-file');
+(async function build() {
+  const files = [
+    './dist/webapp/polyfills-es2015.js',
+    './dist/webapp/runtime-es2015.js',
+    './dist/webapp/main-es2015.js',
+    './dist/webapp/styles-es2015.js',
+  ];
+  await concat(files, `../firefox-addon/src/angularBuild.js`);
+
+  const options = {
+    files: '../firefox-addon/src/angularBuild.js',
+    from: /webpackJsonp/g,
+    to: 'webpackJsonp2',
+  };
+  replace(options);
+})();
